5672409504fac124e75a3f9b8e3eee30b0bb8ae1
Nikolas Howard <nikolas.howard@LighthouseSystems.com>
2022-05-11 08:59:40 +0100

    Added CSS file

--- src/DurationControl.tsx
+++ src/DurationControl.tsx
@@ -3,9 +3,10 @@ import * as React from "react";
 import { DurationControlUnit, DurationControlUnitProps } from "./DurationControlUnit";
 import { Spinner } from "./Spinner";
 
+import "./DurationControl.css";
+
 export type DurationControlProps = React.PropsWithChildren<{
-    pattern: string; // TODO Something optional like 'Days {d} Hours {hh} Minutes {mm} Seconds {SS} Millis {sss}'
-    // ["Days {d}", "Hour {h}"].join(" ")
+    pattern: string; 
 
 	value: number;
 }>;
@@ -18,19 +19,6 @@ export type DurationControlState = {
  * The DurationControl component.
  */
 export class DurationControl extends React.Component<DurationControlProps, DurationControlState> {
-    /** The control styles. */
-    private static _controlStyles: React.CSSProperties = {
-        display: "inline-block"
-    };
-
-    /** The control wrapper styles. */
-    private static  _controlWrapperStyles: React.CSSProperties = {
-        display: "flex",
-	    flexDirection: "row",
-        padding: "2px",
-        border: "1px gray solid"
-    };
-
 	/**
 	 * Creates the DurationControl element.
 	 * @param props The control properties.
@@ -61,13 +49,13 @@ export class DurationControl extends React.Component<DurationControlProps, Durat
 	 */
 	public render(): React.ReactNode {
 		return (
-            <div className="react-duration-control" style={DurationControl._controlStyles}>
-                <div className="control-wrapper" style={DurationControl._controlWrapperStyles}>
+            <div className="react-duration-control">
+                <div className="control-wrapper">
                     <div className="elements-container">
 						<span style={{ whiteSpace: "pre" }}>Hours </span>
-						<input type="text" maxLength={2} value={this.state.value} onChange={(event) => this.setState({ value: parseInt(event.target.value) })} />
+						<input style={{ width: "3em" }} type="text" maxLength={3} value={this.state.value} onChange={(event) => this.setState({ value: parseInt(event.target.value) })} />
 						<span style={{ whiteSpace: "pre" }}> Minutes </span>
-						<input type="text" maxLength={2} value={this.state.value} onChange={(event) => this.setState({ value: parseInt(event.target.value) })} />
+						<input type="text" maxLength={3} value={this.state.value} onChange={(event) => this.setState({ value: parseInt(event.target.value) })} />
                     </div>
                     <Spinner />
                 </div>
