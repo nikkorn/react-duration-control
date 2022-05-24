70b36b15fa1e12f1b6e97ff06a3ae1064f048700
Nikolas Howard <nikolas.howard@LighthouseSystems.com>
2022-05-24 11:00:18 +0100

    Upgraded to React 18

--- example/index.tsx
+++ example/index.tsx
@@ -1,11 +1,11 @@
 import React from "react";
-import ReactDOM from "react-dom";
-import "./index.css";
+import ReactDOM from "react-dom/client";
 import App from "./App";
 
 const root = ReactDOM.createRoot(
     document.getElementById("root") as HTMLElement
 );
+
 root.render(
     <React.StrictMode>
         <App />
