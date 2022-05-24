43c36eed739f4a21fda95da6d7b9ba1391c40134
Nikolas Howard <nikolas.howard@LighthouseSystems.com>
2022-05-24 10:56:40 +0100

    mneed to upgrade to react 18

---
+++ example/App.tsx
@@ -0,0 +1,41 @@
+import React, { useState } from "react";
+import "./App.css";
+
+import { DurationControl } from "../src/DurationControl";
+
+function App() {
+    const [pattern, setPattern] = useState(
+        "Days {dd} Hours {hh} Minutes {mm} Seconds {ss} Millis {fff}"
+    );
+    const [millis, setMillis] = useState(60000);
+
+    return (
+        <div className="App">
+            <DurationControl
+                disabled={false}
+                hideSpinner={false}
+                pattern={pattern}
+                value={millis}
+                onChange={(value) => setMillis(value)}
+            />
+
+            <h3>Pattern</h3>
+            <input
+                size={60}
+                type="text"
+                value={pattern}
+                onChange={(event) => setPattern(event.target.value)}
+            ></input>
+
+            <h3>Millis</h3>
+            <input
+                size={60}
+                type="number"
+                value={millis}
+                onChange={(event) => setMillis(parseInt(event.target.value))}
+            ></input>
+        </div>
+    );
+}
+
+export default App;
