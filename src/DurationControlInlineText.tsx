9a14d3d0b9d37c70c7ec85119e81f8b3ad120c28
Yidah Aguirre Bustos <yidah.aguirrebustos@infor.com>
2024-07-30 15:17:32 +0100

    Added onUnitFocus/Blur and tab functionality.

--- src/DurationControlInlineText.tsx
+++ src/DurationControlInlineText.tsx
@@ -3,25 +3,20 @@ import * as React from "react";
 /**
  * The duration unit types.
  */
-export type DurationUnitType =
-    | "day"
-    | "hour"
-    | "minute"
-    | "second"
-    | "millisecond";
+export type DurationUnitType = "day" | "hour" | "minute" | "second" | "millisecond";
 
 /**
  * The DurationControlInlineText component props.
  */
 export type DurationControlInlineTextProps = {
-    value: string;
+	value: string;
 };
 
 /**
  * The DurationControlInlineText component.
  */
-export const DurationControlInlineText: React.FunctionComponent<
-    DurationControlInlineTextProps
-> = ({ value }) => {
-    return <span className="react-duration-control-inline-text">{value}</span>;
+export const DurationControlInlineText: React.FunctionComponent<DurationControlInlineTextProps> = ({
+	value
+}) => {
+	return <span className="react-duration-control-inline-text">{value}</span>;
 };
