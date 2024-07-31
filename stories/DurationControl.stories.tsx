e4f75e8267be75ed7b8c1a55c4f0fe5d5822cc0a
Yidah Aguirre Bustos <yidah.aguirrebustos@infor.com>
2024-07-31 11:00:17 +0100

    Adjust to review

--- stories/DurationControl.stories.tsx
+++ stories/DurationControl.stories.tsx
@@ -3,25 +3,11 @@ import { ComponentStory, ComponentMeta } from "@storybook/react";
 
 import { DurationControl } from "../src/DurationControl";
 
-const DurationControlWrapper = ({
-	pattern,
-	disabled,
-	hideSpinner,
-	label,
-	onUnitFocus,
-	onUnitBlur
-}) => {
+const DurationControlWrapper = ({ pattern, disabled, hideSpinner, label }) => {
 	const [millis, setMillis] = useState(0);
 
 	return (
 		<div>
-			<input
-				size={60}
-				type="number"
-				value={millis}
-				onChange={(event) => setMillis(parseInt(event.target.value))}
-			></input>
-			<br />
 			<DurationControl
 				disabled={disabled}
 				hideSpinner={hideSpinner}
@@ -29,10 +15,10 @@ const DurationControlWrapper = ({
 				label={label}
 				value={millis}
 				onChange={(value) => setMillis(value)}
-				onUnitFocus={(unit) => onUnitFocus(unit)}
-				onUnitBlur={(unit) => onUnitBlur(unit)}
+				onUnitFocus={(unit) => console.log("Focus", unit)}
+				onUnitBlur={(unit) => console.log("Blur", unit)}
 			/>
-			<br />
+			<h3>Millis</h3>
 			<input
 				size={60}
 				type="number"
@@ -59,7 +45,5 @@ AllUnits.args = {
 	pattern: "Days {dd} Hours {hh} Minutes {mm} Seconds {ss} Millis {fff}",
 	disabled: false,
 	hideSpinner: false,
-	label: "",
-	onUnitFocus: (unit) => console.log("focus", unit),
-	onUnitBlur: (unit) => console.log("blur", unit)
+	label: ""
 };
